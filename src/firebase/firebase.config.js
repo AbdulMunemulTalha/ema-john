7972cb97f315da2fd97787411014bf828badd564
Abdul Munemul Talha <abdulmunemultalhas@gmail.com>
2022-10-25 02:51:45 +0600

    signup and login page designed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDh0ZQV4IQBvmLqedPrIvw-oIUPlQ3CKMU",
+    authDomain: "ema-john-894ae.firebaseapp.com",
+    projectId: "ema-john-894ae",
+    storageBucket: "ema-john-894ae.appspot.com",
+    messagingSenderId: "967382473497",
+    appId: "1:967382473497:web:ff5dab09aa7ffe967e8388"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
